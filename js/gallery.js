b9b092d5c85ac9e928938fcccabcf2781eaa1b02
YanaGuzienko <69dusha69@ukr.net>
2021-01-27 16:08:50 +0200

    исправляет ошибки

--- js/gallery.js
+++ js/gallery.js
@@ -43,14 +43,13 @@ function ulClick(event) {
     return;
   }
   openModal();
-
   refs.imgModal.src = event.target.dataset.source;
   refs.imgModal.alt = event.target.alt;
-  refs.imgModal.setAttribute('data-index', `${event.target.dataset.index}`);
+
   activeIndex = Number(event.target.dataset.index);
 
   window.addEventListener('keydown', nextImg);
-  window.addEventListener('keydown', prevtImg);
+  window.addEventListener('keydown', prevImg);
 }
 
 function openModal() {
@@ -60,36 +59,20 @@ function closeModal() {
   refs.div.classList.remove('is-open');
   refs.imgModal.src = '';
 }
+
 function nextImg(event) {
-  if (event.code === 'ArrowRight') {
+  if (event.code === 'ArrowRight' && activeIndex < images.length - 1) {
     refs.imgModal.setAttribute('src', `${images[activeIndex].original}`);
-    refs.imgModal.src = `${images[activeIndex].original}`;
 
     activeIndex += 1;
-    console.log(refs.imgModal);
   }
-
-  if (activeIndex >= images.length) {
-    activeIndex -= 1;
-    window.removeEventListener('keydown', nextImg);
-    window.addEventListener('keydown', prevtImg);
-  }
-
-  console.log(refs.imgModal);
 }
 
-function prevtImg(event) {
-  if (event.code === 'ArrowLeft') {
-    refs.imgModal.setAttribute('src', `${images[activeIndex].original}`);
-
+function prevImg(event) {
+  if (event.code === 'ArrowLeft' && activeIndex > 0) {
     activeIndex -= 1;
   }
-
-  if (activeIndex < 0) {
-    activeIndex += 1;
-    window.removeEventListener('keydown', prevtImg);
-    window.addEventListener('keydown', nextImg);
-  }
+  refs.imgModal.setAttribute('src', `${images[activeIndex].original}`);
 }
 
 refs.btnModal.addEventListener('click', closeModal);
@@ -100,3 +83,33 @@ window.addEventListener('keydown', event => {
     closeModal();
   }
 });
+
+// function nextImg(event) {
+//   if (event.code === 'ArrowRight') {
+//     refs.imgModal.setAttribute('src', `${images[activeIndex].original}`);
+
+//     activeIndex += 1;
+//   }
+
+//   if (activeIndex >= images.length) {
+//     activeIndex -= 1;
+//     window.removeEventListener('keydown', nextImg);
+//     window.addEventListener('keydown', prevtImg);
+//   }
+
+//   console.log(refs.imgModal.src);
+// }
+
+// function prevImg(event) {
+//   if (event.code === 'ArrowLeft') {
+//     refs.imgModal.setAttribute('src', `${images[activeIndex].original}`);
+
+//     activeIndex -= 1;
+//   }
+
+//   if (activeIndex < 0) {
+//     activeIndex += 1;
+//     window.removeEventListener('keydown', prevtImg);
+//     window.addEventListener('keydown', nextImg);
+//   }
+// }
